fbe70bf5c6b81b6f02da4406c1f5a758b9971fe9
Shriyash <shri12nil@gmail.com>
2021-11-22 14:42:45 +0530

    This is the Hybrid automation Framework for Bigsmall.in

---
+++ Hybrid_Framework/src/main/java/mindtree/Hybrid_Framework/App.java
@@ -0,0 +1,13 @@
+package mindtree.Hybrid_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Big Small Hybrid Automation Framework!" );
+    }
+}
